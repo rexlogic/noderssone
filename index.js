339743663abfb5781f9d0c259f6466b97f491740
rexlogic <40705463+rexlogic@users.noreply.github.com>
2020-05-19 18:50:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const app = express()
 let Parser = require('rss-parser')
 let parser = new Parser()
 
+var linkone = ''
+
 app.use(bodyParser.json())
 app.set('port', (process.env.PORT || 5000))
 
 parser.parseURL('https://www.reddit.com/.rss', function(err, feed) {
   if (err) throw err;
   console.log(feed.title);
+  linkone = feed.items[0].link;
+  console.log('L1=' + linkone);
   feed.items.forEach(function(entry) {
     console.log(entry.title + ':' + entry.link);
   })
